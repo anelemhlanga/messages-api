f38918ca36f2278d2de163a082ee1469d6a78a03
Anele Mhlanga <anelenmhlanga@gmail.com>
2022-12-15 19:10:15 +0200

    add argument decorators

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @Get()
+  getMessages() {}
+
+  @Post()
+  postMessages(@Body() body: any) {
+    console.debug('body', body);
+  }
+
+  @Get('/:id')
+  getMessageById(@Param('id') id: string) {
+    console.debug('id,', id);
+  }
+}
